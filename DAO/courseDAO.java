ebeccfb91e2c9de4c2f85f38432bb6ef4ae82d20
sz0701 <96040958+sz0701@users.noreply.github.com>
2024-04-05 14:25:01 +0800

    Add files via upload

---
+++ DAO/courseDAO.java
@@ -0,0 +1,37 @@
+package DAO;
+
+import adt.ListInterface;
+import entity.Course;
+import java.io.*;
+import adt.ArrayList;
+
+public class courseDAO {
+
+    private final String fileName;
+
+    public courseDAO(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void saveToFile(ListInterface<Course> courseList) {
+        try (ObjectOutputStream ooStream = new ObjectOutputStream(new FileOutputStream(fileName))) {
+            ooStream.writeObject(courseList);
+            System.out.println("Data saved successfully.");
+        } catch (IOException ex) {
+            System.err.println("Error saving data to file: " + ex.getMessage());
+        }
+    }
+
+    public ListInterface<Course> retrieveFromFile() {
+        ListInterface<Course> courseList = new ArrayList<>();
+        try (ObjectInputStream oiStream = new ObjectInputStream(new FileInputStream(fileName))) {
+            courseList = (ArrayList<Course>) oiStream.readObject();
+            System.out.println("Data retrieved successfully.");
+        } catch (FileNotFoundException ex) {
+            System.err.println("File not found: " + ex.getMessage());
+        } catch (IOException | ClassNotFoundException ex) {
+            System.err.println("Error reading data from file: " + ex.getMessage());
+        }
+        return courseList;
+    }
+}
