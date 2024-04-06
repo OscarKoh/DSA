ef53f3a807058498368d12aa8cb6d09546e2162b
sz0701 <96040958+sz0701@users.noreply.github.com>
2024-04-07 00:08:09 +0800

    DataInitializer.java

--- init/DataInitializer.java
+++ init/DataInitializer.java
@@ -10,6 +10,7 @@ public class DataInitializer {
     public static void initializeAllData() {
         initializeProgramme();
         initializeCourse();
+
     }
 
     public static void initializeProgramme() {
@@ -47,22 +48,18 @@ public class DataInitializer {
     }
 
     public static void initializeCourse() {
+        // Course data
         String[] courseCodes = {
             "BACS2023", "BACS1053", "BACS2063", "BAIT1023", "BAIT1043",
-            "BBBE2013", "BBDT2123",  "BAIT2073", "BAIT1093", 
+            "BBBE2013", "BBDT2123", "BAIT2073", "BAIT1093",
             "BAIT2012"
         };
         String[] courseNames = {
             "Object-Oriented Programming", "Database Management", "Data Structure and Algorithms",
             "Web Design and Development", "System Analysis And Design", "Money And Banking",
-            "Marketing Of Financial Services", "English For Career Preparation", 
-            "Mobile Application Development", "Introduction To Computer Security",
-            
-        };
-        String[] statuses = {
-            "Main",  "Main", "Resit", "Repeat",
-            "Main", "Resit",  "Repeat", "Elective", "Elective",  "Main"
-        };
+            "Marketing Of Financial Services", "English For Career Preparation",
+            "Mobile Application Development", "Introduction To Computer Security",};
+       
         // Define course types for each course
         ArrayList<Course.CourseType>[] courseTypeArrays = new ArrayList[courseCodes.length];
         for (int i = 0; i < courseCodes.length; i++) {
@@ -72,7 +69,7 @@ public class DataInitializer {
                 courseTypeArrays[i].add(Course.CourseType.LECTURE);
                 courseTypeArrays[i].add(Course.CourseType.TUTORIAL);
                 courseTypeArrays[i].add(Course.CourseType.PRACTICAL);
-            } else if (i == 6 && i ==7) {
+            } else if (i == 6 && i == 7) {
                 // Courses with lecture and tutorial
                 courseTypeArrays[i].add(Course.CourseType.LECTURE);
                 courseTypeArrays[i].add(Course.CourseType.TUTORIAL);
@@ -88,10 +85,11 @@ public class DataInitializer {
             449.00, 445.00, 700.00, 800.00, 560.00, 780.00, 903.00,
             670, 899, 995, 555, 677, 777
         };
-        
+
         // Loop through each course and initialize it
         for (int i = 0; i < courseCodes.length; i++) {
-            ManageCourse.addCourse(courseCodes[i], courseNames[i], statuses[i], 3, courseTypeArrays[i], fees[i]);
+            ManageCourse.addCourse(courseCodes[i], courseNames[i],  3, courseTypeArrays[i], fees[i]);
         }
     }
+
 }
