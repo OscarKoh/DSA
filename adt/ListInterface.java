781cdb4d31ce2f4aaa33e2e02e77ccf56436405c
sz0701 <96040958+sz0701@users.noreply.github.com>
2024-04-10 03:17:03 +0800

    ListInterface.java

--- adt/ListInterface.java
+++ adt/ListInterface.java
@@ -1,68 +1,35 @@
 package adt;
 
 import java.util.Iterator;
-import java.util.Set;
 
-/**
- * ListInterface.java An interface for the ADT List. Entries in the list have
- * positions that begin with 1.
- *
- * @author Frank M. Carrano
- * @version 2.0
- */
 public interface ListInterface<T> {
 
-    /**
-     * Task: Adds a new entry to the end of the list. Entries currently in the
-     * list are unaffected. The list's size is increased by 1.
-     *
-     * @param newEntry the object to be added as a new entry
-     * @return true if the addition is successful, or false if the list is full
-     */
-    public boolean add(T newEntry);
-
-    public void amend(int index, T newItem);
+    public void add(T newItem);
 
     public T remove(int index);
 
-    public boolean remove(T element);
-
-    /**
-     * Task: Removes all entries from the list.
-     */
-    public void clear();
-
-
-    /**
-     * Task: Sees whether the list contains a given entry.
-     *
-     * @param anEntry the object that is the desired entry
-     * @return true if the list contains anEntry, or false if not
-     */
-    public boolean contains(T anEntry);
-
-    public boolean isFull();
-
-    public void mergeSets(Set<T> anotherSet);
-
-    public T[] toArray();
-
-    public void generateSummaryReports();
-
-    /**
-     * Task: Sees whether the list is empty.
-     *
-     * @return true if the list is empty, or false if not
-     */
-    public boolean isEmpty();
+    public void amend(int index, T newItem);
 
     public int search(T item);
 
-    public T get(int index);
+    public Iterator<T> getIterator();
 
-    public int indexOf(T element);
+    public void mergeSets(ListInterface<T> anotherSet);
+
+    public boolean isEmpty();
 
     public int size();
 
-    public Iterator<T> getIterator();
+    public T get(int index);
+
+    public void clear();
+
+    public T[] toArray();
+
+    public void sort();
+
+    public void sort(String... atributeNames);
+
+    public void swap(int firstIndex, int secIndex);
+
 }
