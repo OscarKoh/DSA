962619eeb1ebb6e286a8c709d8fff38664cc8644
owyqqqq <104346357+owyqqqq@users.noreply.github.com>
2024-04-16 23:07:14 +0800

    Update Student.java

--- entity/Student.java
+++ entity/Student.java
@@ -7,26 +7,41 @@ package entity;
 import adt.ListInterface;
 import adt.ArrayList;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author Ow Yong Qing
  */
 public class Student implements Serializable {
+
     private int studentID;
     private String name;
     private String IC;
     private String contact_number;
+    private String programme;
     private ListInterface<Course> courses = new ArrayList<>();
-    
-    public Student(){
+    private ListInterface<RegisteredCourse> registeredCourses;
+
+
+    public Student() {
     }
-    
-    public Student(int studentID, String name, String IC, String contact_number){
+
+    public Student(int studentID, String name, String IC, String contact_number, String programme) {
         this.studentID = studentID;
         this.name = name;
         this.IC = IC;
         this.contact_number = contact_number;
+        this.programme = programme;
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
     }
 
     public ListInterface<Course> getCourses() {
@@ -37,6 +52,14 @@ public class Student implements Serializable {
         this.courses = courses;
     }
 
+    public ListInterface<RegisteredCourse> getRegisteredCourses() {
+        return registeredCourses;
+    }
+
+    public void setRegisteredCourses(ListInterface<RegisteredCourse> registeredCourses) {
+        this.registeredCourses = registeredCourses;
+    }
+
     public String getIC() {
         return IC;
     }
@@ -44,11 +67,11 @@ public class Student implements Serializable {
     public void setIC(String IC) {
         this.IC = IC;
     }
-    
+
     public int getStudentID() {
         return studentID;
     }
-    
+
     public String getName() {
         return name;
     }
@@ -68,9 +91,34 @@ public class Student implements Serializable {
     public void setContact_number(String contact_number) {
         this.contact_number = contact_number;
     }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Student other = (Student) obj;
+        return this.studentID == other.studentID;
+    }
+
+    @Override
+    public int hashCode() {
+        // Calculating the hash code based on all fields
+        int result = studentID;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (IC != null ? IC.hashCode() : 0);
+        result = 31 * result + (contact_number != null ? contact_number.hashCode() : 0);
+        result = 31 * result + (programme != null ? programme.hashCode() : 0);
+        return result;
+    }
 
     @Override
     public String toString() {
-        return String.format("  %-10s %-30s %-15s %-13s", studentID, name, IC, contact_number);
-    }   
+        return String.format("  %-10s %-30s %-15s %-13s %-30s", studentID, name, IC, contact_number, programme);
+    }
 }
