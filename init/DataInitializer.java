089683dc144c3c65e1ae4a0189094281e57707b5
owyqqqq <104346357+owyqqqq@users.noreply.github.com>
2024-04-16 23:12:29 +0800

    Update DataInitializer.java

--- init/DataInitializer.java
+++ init/DataInitializer.java
@@ -4,13 +4,17 @@ import control.ManageProgramme;
 import control.ManageCourse;
 import adt.ArrayList;
 import entity.Course;
+import entity.RegisteredCourse;
+import control.ManageRegistered;
+import control.ManageStudent;
 
 public class DataInitializer {
 
     public static void initializeAllData() {
         initializeProgramme();
         initializeCourse();
-
+        initializeStudent();
+        initializeRegistration();
     }
 
     public static void initializeProgramme() {
@@ -91,6 +95,92 @@ public class DataInitializer {
         // Loop through each course and initialize it
         for (int i = 0; i < courseCodes.length; i++) {
             ManageCourse.addCourse(courseCodes[i], courseNames[i],  3, courseTypeArrays[i], fees[i]);
+        }
+    }
+
+    public static void initializeStudent() {
+        int[] stuID = {
+            1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012
+        };
+
+        String[] stuname = {
+            "Eric Tsen",
+            "Melissa Wong",
+            "Yew Hong Yin",
+            "Chan Pei Yee",
+            "Cathy Loke",
+            "Kelly Ong",
+            "Kevin Kwa",
+            "John Doe",
+            "Steve Ong",
+            "Stephie Yong",
+            "Sean Ong",
+            "Jaydon Wong"
+        };
+
+        String[] stuIC = {
+            "981025-14-5638",
+            "750617-05-9203",
+            "830926-06-1574",
+            "690817-08-3412",
+            "480530-11-8956",
+            "571204-03-6721",
+            "891211-09-4465",
+            "630712-12-3789",
+            "720309-02-5436",
+            "561110-07-2894",
+            "471013-10-7542",
+            "840218-01-6329"
+        };
+
+        String[] stucontact_number = {
+            "011-3498910",
+            "012-4982380",
+            "019-4582309",
+            "012-3596079",
+            "018-3498123",
+            "012-7793245",
+            "017-2487298",
+            "012-7793223",
+            "012-3476291",
+            "018-1345623",
+            "017-7793355",
+            "012-7153647"
+        };
+
+        String[] stuProgramme = { // Change to array of programme codes
+            "RAC", "RDS", "RSW", "RDS", "RRT", "RET", "DLS", "DMA", "DMA", "RRM", "DME", "RAC"
+        };
+        
+        for (int i = 0; i < stuname.length; i++) {
+            ManageStudent.addStudent(stuID[i], stuname[i], stuIC[i], stucontact_number[i], stuProgramme[i]);
+        }
+    }
+
+    public static void initializeRegistration() {
+        RegisteredCourse[] regCourses = {
+            new RegisteredCourse(1001, "BBBE2013", "Main"),
+            new RegisteredCourse(1001, "BBDT2123", "Elective"),
+            new RegisteredCourse(1001, "BAIT2073", "Resit"),
+            new RegisteredCourse(1002, "BACS1053", "Main"),
+            new RegisteredCourse(1002, "BACS2063", "Resit"),
+            new RegisteredCourse(1003, "BACS2063", "Elective"),
+            new RegisteredCourse(1003, "BAIT1093", "Repeat"),
+            new RegisteredCourse(1003, "BAIT2012", "Repeat"),
+            new RegisteredCourse(1004, "BACS1053", "Repeat"),
+            new RegisteredCourse(1004, "BACS2063", "Main"),
+            new RegisteredCourse(1005, "BACS2023", "Resit"),
+            new RegisteredCourse(1006, "BBBE2013", "Repeat"),
+            new RegisteredCourse(1006, "BBDT2123", "Elective"),
+            new RegisteredCourse(1007, "BAIT2073", "Elective"),
+            new RegisteredCourse(1008, "BBDT2123", "Resit"),
+            new RegisteredCourse(1009, "BBDT2123", "Main"),
+            new RegisteredCourse(1010, "BBBE2013", "Resit"),
+            new RegisteredCourse(1011, "BAIT2073", "Elective"),
+            new RegisteredCourse(1012, "BBDT2123", "Main")};
+
+        for (RegisteredCourse regCourse : regCourses) {
+            ManageRegistered.addRegistered(regCourse);
         }
     }
 
